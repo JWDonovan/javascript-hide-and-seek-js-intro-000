105e577e7753ae0d60e5e64b457278f5daa6888d
James Donovan <jameswilliamdonovan@gmail.com>
2020-10-19 02:51:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,16 @@ function getFirstSelector(selector) {
 function nestedTarget() {
   return document.querySelector('#nested').querySelector('.target');
 }
+
+function deepestChild() {
+  // deepest child of #grand-node
+}
+
+function increaseRankBy(n) {
+  let rankedList = document.getElementsByClassName('ranked-list');
+  rankedList.forEach(element => {
+    rank = parseInt(element.children[i].innerHTML);
+    rank += n;
+    element.children[i].innerHTML = rank;
+  });
+}
